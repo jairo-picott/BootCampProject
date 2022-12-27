b54054e0605fa88eeb014c483ecc7705178cc3b5
jairo-picott <jairo.picott@casais.pt>
2022-12-27 17:44:10 +0000

    First Commit - Ejercicios tema 3
    FirstCommint

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(5, 6, 8);
+    }
+
+    public static void suma(int a, int b, int c) {
+        int d = a+b+c;
+        System.out.println("El resultado es " + d);
+    }
+}
