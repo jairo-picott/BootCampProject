352ac9b080dae9b0b2ad3d0f8556cf603880a9d8
jairo-picott <jairo.picott@casais.pt>
2022-12-27 17:53:40 +0000

    Agregar Segundo Punto

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         suma(5, 6, 8);
+        Coche miCoche = new Coche();
+        miCoche.agregarPuerta();
+        System.out.println(miCoche.puertas);
     }
 
     public static void suma(int a, int b, int c) {
         int d = a+b+c;
         System.out.println("El resultado es " + d);
     }
+}
+
+class Coche {
+    public int puertas = 0;
+
+    public void agregarPuerta() {
+        this.puertas++;
+    }
+
 }
